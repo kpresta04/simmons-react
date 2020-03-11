09c1b5c7de538452f57b1172b28fe50a26bd893a
Kellen Presta <kpresta04@gmail.com>
2020-03-11 18:11:49 -0500

    Header base styles done

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,24 +7,31 @@ const Header = () => (
       <NavLink to="/" activeClassName="is-active" exact={true}>
         <img src="./simmons-logo-branding-1B-3.png"></img>
       </NavLink>
-      <NavLink to="/shop" activeClassName="is-active">
-        Shop
-      </NavLink>
+      <div id="siteLinks">
+        <NavLink to="/shop" activeClassName="is-active">
+          Shop
+        </NavLink>
 
-      <NavLink to="/services" activeClassName="is-active">
-        Services
-      </NavLink>
-      <NavLink to="/contact" activeClassName="is-active">
-        Contact
-      </NavLink>
-      <NavLink to="/login" activeClassName="is-active">
-        Login
-      </NavLink>
-      <NavLink to="/cart" activeClassName="is-active">
-        Cart
-      </NavLink>
+        <NavLink to="/services" activeClassName="is-active">
+          Services
+        </NavLink>
+        <NavLink to="/about" activeClassName="is-active">
+          About
+        </NavLink>
+        <NavLink to="/contact" activeClassName="is-active">
+          Contact
+        </NavLink>
+      </div>
+      <div id="accountLinks">
+        <NavLink to="/login" activeClassName="is-active">
+          Login
+        </NavLink>
+        <NavLink to="/cart" activeClassName="is-active">
+          Cart
+        </NavLink>
+      </div>
     </header>
   </div>
 );
-// shop - serverices - about - contact
+
 export default Header;
