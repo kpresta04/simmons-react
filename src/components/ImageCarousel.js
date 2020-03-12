21f2a95a560ecfd42fe756b1363afb84fdb4762b
Kellen Presta <kpresta04@gmail.com>
2020-03-12 12:49:18 -0500

    descripts

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -17,17 +17,25 @@ class ImageCarousel extends React.Component {
           <div className="legend">
             <h3>In case you haven't heard...</h3>
             <h1>Simmons Restores Firearms</h1>
+            <button>Browse Our Services</button>
           </div>
         </div>
         <div>
           <img src="./carousel/pistols.jpg" />
-          <p className="legend">Legend 1</p>
+          <div className="legend">
+            <h3>Since 1945</h3>
+            <h1>We Sell Shotguns, rifles, and pistols - all brands</h1>
+            <button>Shop Now</button>
+          </div>
         </div>
 
         <div>
           <img src="./carousel/rifleScope.jpg" />
-
-          <p className="legend">Legend 2</p>
+          <div className="legend">
+            <h3>Repairs and Upgrades</h3>
+            <h1>Professional gunsmithing with over 75 years of history!</h1>
+            <button>Contact Us</button>
+          </div>
         </div>
       </Carousel>
     );
