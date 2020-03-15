4d5b4b24ab87eb7ca20276be613cf14807dbfe87
Kellen Presta <kpresta04@gmail.com>
2020-03-15 17:57:55 -0500

    adding footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <div>
+    <footer className="footer">
+      <p>Hello</p>
+    </footer>
+  </div>
+);
+
+export default Footer;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,6 +6,7 @@ import EditPage from "../components/EditExpensePage";
 import HelpPage from "../components/HelpPage";
 import NotFoundPage from "../components/NotFoundPage";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -18,6 +19,7 @@ const AppRouter = () => (
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
+      <Footer />
     </div>
   </BrowserRouter>
 );
