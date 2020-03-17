62c0e66177f31c50e0fa0bae0f9c4ea8e1c79f6e
Kellen Presta <kpresta04@gmail.com>
2020-03-17 18:19:52 -0500

    main

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import { connect } from "react-redux";
 import ImageCarousel from "./ImageCarousel";
+import MainContent from "./MainContent";
 
 const ExpenseDashboardPage = () => (
   <div>
     <ImageCarousel />
+    <MainContent />
   </div>
 );
 
