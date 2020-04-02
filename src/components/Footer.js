b10113bf762687ddfc2848d50026e9fa111e7c9e
Kellen Presta <kpresta04@gmail.com>
2020-04-01 23:44:01 -0500

    footer improved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
 	<div>
 		<footer className="footer">
 			<p>Copyright 2020 © Simmons Gun Repair</p>
-			<NavLink to="/privacy">Privacy Policy</NavLink>
+			<Link to="/privacy">Privacy Policy</Link>
 		</footer>
 	</div>
 );
