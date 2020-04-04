acafe3860a1d35a1c24bf2ef217524ad5cbd8487
Kellen Presta <kpresta04@gmail.com>
2020-04-03 21:08:49 -0500

    about page p text wrapping

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -12,17 +12,20 @@ const AboutPage = () => (
 				></img>
 			</div>
 
+			<div>
+				<img
+					className="smallImage"
+					src="./images/Ernie.s-3-280x280.jpg"
+					alt="Pop Simmons"
+				></img>
+			</div>
+
 			<p>
 				Ernie “Pop” Simmons started a gun repair shop in 1945. Two years later
 				he invented and patented the Simmons “Floating” Vent Rib for shotguns to
 				improve the aim of all types of sportsmen. Over 70 years later, Simmons
 				is still installing vent ribs and trying to improve your firearms
 				performance in any way we can.
-				<img
-					className="smallImage"
-					src="./images/Ernie.s-3-280x280.jpg"
-					alt="Pop Simmons"
-				></img>
 			</p>
 		</div>
 	</div>
