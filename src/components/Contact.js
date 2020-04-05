a634cc9128b2a72ea29be353ce840559434fa8d9
Kellen Presta <kpresta04@gmail.com>
2020-04-05 15:06:57 -0500

    contact form first draft

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,24 @@ import FAQ from "./FAQ";
 const Contact = () => (
 	<div className="main">
 		<FAQ />
+		<div id="contact">
+			<h3>Get in Touch</h3>
+
+			<form
+				id="contactForm"
+				onSubmit={(e) => {
+					e.preventDefault();
+				}}
+			>
+				<input type="text" placeholder="NAME" />
+				<input type="email" placeholder="EMAIL" />
+				<textarea placeholder="YOUR MESSAGE" cols="30" rows="10"></textarea>
+				<button id="sendBtn">Send </button>
+			</form>
+			<button>
+				<Link to="/">Back To Simmons</Link>
+			</button>
+		</div>
 	</div>
 );
 
