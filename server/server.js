416b50b3c4bbee4b0164c0c055392e1335236b03
Kellen Presta <kpresta04@gmail.com>
2020-04-07 19:20:04 -0500

    added compression

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const compression = require("compression");
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+app.use(compression());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(port, () => {
-  console.log('Server is up!');
+	console.log("Server is up!");
 });
