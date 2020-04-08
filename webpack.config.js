5cebff9bda782c4c1507fec688065065cbc0484a
Kellen Presta <kpresta04@gmail.com>
2020-04-07 19:33:55 -0500

    added brotli

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,69 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrotliGzipPlugin = require("brotli-gzip-webpack-plugin");
 
-module.exports = env => {
-  const isProd = env === "production";
+module.exports = (env) => {
+	const isProd = env === "production";
 
-  return {
-    entry: "./app.js",
-    output: {
-      path: path.join(__dirname, "public"),
-      filename: "bundle.js"
-    },
+	return {
+		entry: "./app.js",
+		output: {
+			path: path.join(__dirname, "public"),
+			filename: "bundle.js",
+		},
 
-    plugins: [new MiniCssExtractPlugin()],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-react"],
-              plugins: ["@babel/plugin-proposal-class-properties"]
-            }
-          }
-        },
-        {
-          test: /\.s?css$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                esModule: true
-              }
-            },
-            { loader: "css-loader", options: { sourceMap: true } },
-            { loader: "sass-loader", options: { sourceMap: true } }
-          ]
-        }
-      ]
-    },
-    devtool: isProd ? "source-map" : "inline-source-map",
-    devServer: {
-      contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true
-    }
-  };
+		plugins: [
+			new MiniCssExtractPlugin(),
+			new BrotliGzipPlugin({
+				asset: "[path].br[query]",
+				algorithm: "brotli",
+				test: /\.(js|css|html|svg)$/,
+				threshold: 10240,
+				minRatio: 0.8,
+				quality: 11,
+			}),
+			new BrotliGzipPlugin({
+				asset: "[path].gz[query]",
+				algorithm: "gzip",
+				test: /\.(js|css|html|svg)$/,
+				threshold: 10240,
+				minRatio: 0.8,
+			}),
+		],
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-react"],
+							plugins: ["@babel/plugin-proposal-class-properties"],
+						},
+					},
+				},
+				{
+					test: /\.s?css$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+							options: {
+								esModule: true,
+							},
+						},
+						{ loader: "css-loader", options: { sourceMap: true } },
+						{ loader: "sass-loader", options: { sourceMap: true } },
+					],
+				},
+			],
+		},
+		devtool: isProd ? "source-map" : "inline-source-map",
+		devServer: {
+			contentBase: path.join(__dirname, "public"),
+			historyApiFallback: true,
+		},
+	};
 };
 
 // module.exports = {
